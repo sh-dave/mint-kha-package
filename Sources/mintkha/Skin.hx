019cae3f2f06301dd1414fa8d31e16d9ad5d2697
David Klein <dave@smokinheads.de>
2016-01-13 17:07:54 +0100

    html5 demo

--- Sources/mintkha/Skin.hx
+++
@@ -1,5 +0,0 @@
-package mintkha;
-
-typedef Skin = {
-	function drawG2( g : kha.graphics2.Graphics, x : Float, y : Float, w : Float, h : Float ) : Void;
-}
--- build-01-install-dependencies.cmd
+++
@@ -1,3 +0,0 @@
-git clone --recursive https://github.com/KTXSoftware/Kha
-haxelib git mint https://github.com/sh-dave/mint
-::git clone https://github.com/snowkit/mint Libraries/mint
